aa6259100b2bac3ccfb732539b50912af93d4cb9
Rahul25052002 <rnikale55@gmail.com>
2022-09-19 12:32:54 +0530

    Added multiply.c

---
+++ TPPG/multply.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int x;
+    int y;
+    int z=0;
+    printf("Enter the first number\n");
+    scanf("%d",&x);
+    printf("Enter the second number\n");
+    scanf("%d",&y);
+
+    z=x*y;
+
+    printf("Multiplication of two number is \n");
+    printf("%d\n",z);
+}
+
